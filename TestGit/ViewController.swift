4fb7b8ec114a4428ff0c82132ae7291f9e4dd8c3
Rob In der Maur <ridmaur@me.com>
2020-06-21 10:32:51 +0200

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Rob In der Maur on 21/06/2020.
+//  Copyright © 2020 Rob In der Maur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
